2b1c1a537aac7d2cc62eac66e86b1a0045628cd0
TrongNguyen03 <trongnguyenj3@gmail.com>
2022-08-16 09:44:39 +0700

    btvn

---
+++ BTVN/btvn7/b7.js
@@ -0,0 +1,14 @@
+//bài 1
+let getting =('Hello. My name is John');
+console.log('getting charat:',getting.charAt(8));
+
+//bài 2
+let str1 =('the morning is upon us.');
+console.log('str1 slice:',str1.slice(12,22));
+
+//bài 3
+let str2 ='Hello';
+let str3 ='world';
+let str4;
+str4 =`${str2} ${str3}`;
+console.log(str4);
